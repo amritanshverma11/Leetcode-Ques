84dd6d090d73d9095da4b5027495949240eef785
amritanshverma11 <amritansh.g.verma@gmail.com>
2026-09-06 00:20:21 +0530

    Time: 4 ms (88.55%), Space: 133.3 MB (14.89%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstStableIndex(int[] n, int k) {
+        int l=n.length;
+        int m[]=new int [l];
+        m[l-1]=n[l-1];
+        for (int i=l-2;i>=0;i--)
+            m[i]=(n[i]<m[i+1])?n[i]:m[i+1];
+            int mm=Integer.MIN_VALUE;
+        for (int i=0;i<l;i++)
+        {
+            if (n[i]>mm)
+            mm=n[i];
+            if(mm-m[i]<=k)
+            return i;
+        }
+        return -1;
+    }
+}
