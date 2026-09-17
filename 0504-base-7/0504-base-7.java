ce4a55cf86789d7555c910a846922519f969e1ed
amritanshverma11 <amritansh.g.verma@gmail.com>
2026-09-17 23:43:35 +0530

    Time: 1 ms (72.91%), Space: 42.7 MB (59.18%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -2,7 +2,7 @@ class Solution {
     public String convertToBase7(int n) {
         if (n==0)return "0";
        int i=(n<0)?-1:1;
-       n=Math.abs(n);
+       n=(n<0)?(n*-1):n;
         StringBuilder s= new StringBuilder();
         while (n!=0)
         {
