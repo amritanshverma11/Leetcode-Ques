082ed8355795e62142f8ed6273437a2ccc16c7bc
amritanshverma11 <amritansh.g.verma@gmail.com>
2026-09-22 21:43:11 +0530

    Time: 2090 ms (48.13%), Space: 42 MB (62.67%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkPerfectNumber(int n) {
+
+       int s=1;
+       for (int i=2; i<=n/2;i++)
+       {
+          if (n%i==0)
+          s+=i;
+       } 
+       return (n==1)?false:s==n;
+    }
+}
