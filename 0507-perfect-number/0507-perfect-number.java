73b55ba83f78e7263b9bf00f051f08737708825c
amritanshverma11 <amritansh.g.verma@gmail.com>
2026-09-22 21:45:33 +0530

    Time: 416 ms (66.37%), Space: 42.4 MB (10.07%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean checkPerfectNumber(int n) {
-
+        if(n%10 != 8 && n%10 != 6)
+            return false;
        int s=1;
        for (int i=2; i<=n/2;i++)
        {
           if (n%i==0)
           s+=i;
        } 
-       return (n==1)?false:s==n;
+       return s==n;
     }
 }
